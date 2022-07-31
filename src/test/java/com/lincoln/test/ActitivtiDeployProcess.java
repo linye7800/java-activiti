aba649a4738b27abe696b79aae3e8ac85898378f
linye <linye102@163.com>
2022-07-31 23:09:59 +0800

    activiti 通过zip包的方式上传流程

--- src/test/java/com/lincoln/test/ActitivtiDeployProcess.java
+++ src/test/java/com/lincoln/test/ActitivtiDeployProcess.java
@@ -1,9 +1,10 @@
 package com.lincoln.test;
 
-import com.sun.deploy.resources.Deployment;
+import java.io.InputStream;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Deployment;
 import org.junit.Test;
 
 import java.util.zip.ZipInputStream;
@@ -20,11 +21,25 @@ public class ActitivtiDeployProcess {
      *
      * 1. 获取流程引擎
      * 2. 获取repositoryService
+     *
+     * 一个思考
      */
     @Test
     public void deployProcessByZip() {
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         RepositoryService repositoryService = processEngine.getRepositoryService();
+        // 读取资源包文件，构成inputStream
+        InputStream inputStream = this.getClass()
+                .getClassLoader()
+                .getResourceAsStream("bpmn/trip.zip");
+        // 用inputStream 构造ZipInputStream
+        ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+        Deployment deployment = repositoryService.createDeployment()
+                .addZipInputStream(zipInputStream)
+                .deploy();
+
+        System.out.println("流程部署的id=" + deployment.getId());
+        System.out.println("流程部署的name=" + deployment.getName());
     }
 
 }
